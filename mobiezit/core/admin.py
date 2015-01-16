ac114fb3cd9df99c747e81db66659017a7bba37a
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2015-01-16 10:56:42 -0300

    Add new models

--- mobiezit/core/admin.py
+++ mobiezit/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import Movie, UserMovie
+from core.models import Movie, UserMovie, Recommend, Followers
 
 # Register your models here.
 
@@ -7,3 +7,5 @@ from core.models import Movie, UserMovie
 # Register the model and the custom admin modification
 admin.site.register(Movie)
 admin.site.register(UserMovie)
+admin.site.register(Recommend)
+admin.site.register(Followers)
--- mobiezit/core/models.py
+++ mobiezit/core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Movie(models.Model):
     imdb_link = models.URLField(max_length=100, unique=True)
-    user = models.ForeignKey(User)
+    add_by = models.ForeignKey(User)
 
     def __str__(self):
         return self.imdb_link
@@ -14,3 +14,14 @@ class UserMovie(models.Model):
     movie = models.ForeignKey(Movie)
     user = models.ForeignKey(User)
     viewed = models.BooleanField(default=False)
+
+
+class Recommend(models.Model):
+    movie = models.ForeignKey(Movie)
+    recommender = models.ForeignKey(User, related_name="user_recommender")
+    recommended = models.ForeignKey(User, related_name="user_recommended")
+
+
+class Followers(models.Model):
+    follower = models.ForeignKey(User, related_name="user_follower")
+    following = models.ForeignKey(User, related_name="user_following")
