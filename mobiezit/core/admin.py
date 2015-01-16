6bf194bca696d6aab33ff63afa2ccecd493bfeab
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2015-01-16 10:27:22 -0300

    First attemp to define models

--- mobiezit/core/admin.py
+++ mobiezit/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Movie, UserMovie
 
 # Register your models here.
+
+
+# Register the model and the custom admin modification
+admin.site.register(Movie)
+admin.site.register(UserMovie)
--- mobiezit/core/models.py
+++ mobiezit/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Movie(models.Model):
+    imdb_link = models.URLField(max_length=100, unique=True)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.imdb_link
+
+
+class UserMovie(models.Model):
+    movie = models.ForeignKey(Movie)
+    user = models.ForeignKey(User)
+    viewed = models.BooleanField(default=False)
